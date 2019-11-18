e176f797ad94ce2a826cf3ff2897f771ae0ba527
Richard Todd <rwtodd@users.noreply.github.com>
2019-11-18 02:21:46 -0600

    brought the pwsh version in line with the java/c# versions

--- djvu-metadata/gen-djvu-dsed.ps1
+++ djvu-metadata/gen-djvu-dsed.ps1
@@ -1,26 +1,55 @@
+<#
+.Synopsis
+Converts an input file into a djvused script.
+
+.Example
+gen-djvu-dsed.ps1 marks.txt
+#>
 Param(
   [Parameter(Mandatory=$true)] [System.IO.FileInfo] $src
 )
 
-$prefix = 'p'            # prefix for page titles
-$numeric = $false        # are the page titles numeric? 
-[Int32] $djvuPage = -1   # the current djvu page
-$bookPage = -1           # the current book page (may not be numeric)
+$djvuPages = @{ }        # hash from book page names to djvu pages
+[Int32] $djvuPage = -1   # the last recorded djvuPage
+$bookPre = ""           # the last recorded book page, prefix part
+[Int32]$bookNum = -1    # the last recorded book page, numeric part
 
+$marks = [System.Collections.ArrayList]@()       # tracks the bookmarks we've seen
 $metaFile = [System.Text.StringBuilder]::new()   # holds metadata
-$marksFile = [System.Text.StringBuilder]::new()  # holds bookmarks
-$pagesFile = [System.Text.StringBuilder]::new()  # holds page info
+$pagesFile = [System.Text.StringBuilder]::new()  # holds all our output
 
-function Write-Page-Title {
-  $pfx = "" ; if($numeric) { $pfx = $prefix }
-  [void]$pagesFile.AppendLine("select $djvuPage; set-page-title `"$pfx$bookPage`"")
-  $script:djvuPage++
-  if($numeric) { ([Int32] $script:bookPage)++ }
+# helper function to increase $djvuPage and $bookNum in tandem
+function Inc-LastPage {
+  if ($script:djvuPage -ge 1) {
+    $script:djvuPage += 1
+    if ($script:bookNum -eq -1) {
+       $script:bookNum = 2
+    } else {
+       $script:bookNum += 1
+    }
+  }
+}
+
+# Helper function to divide a bookpage into its prefix and suffix
+function Split-BookPage {
+   Param($bp)
+   switch -Regex ($bp) {
+      '^\d+$' {
+          $ans = @("",$matches[0])
+      }
+      '^\S*\D$' {
+          $ans = @($matches[0],-1)
+      }
+      '^(\S*\D)(\d+)$' {
+          $ans = @($matches[1],[int]$matches[2])
+      }
+   }
+   return $ans
 }
 
 switch -Regex -File $src {
   # comment line or blank line
-  '^\s*#|^\s*$' {   }
+  '^\s*#|^\s*$' { }
 
   # Metadata
   '^\s*[Mm]eta\s+([^:]+):\s+(.*?)\s*$' {
@@ -28,30 +57,34 @@ switch -Regex -File $src {
   }
 
   # Page title
-  '^\s*[Dd]jvu\s+(\d+)\s+(?:=|is)\s+[Bb]ook\s+(\S+)(?:\s+[Pp]refix\s+(.*?))?\s*$' {
+  '^\s*[Dd]jvu\s+(\d+)\s+(?:=|is)\s+[Bb]ook\s+(\S+)\s*$' {
     [Int32] $tgtPage = $matches[1]
-    if ($tgtPage -lt $djvuPage) { 
+    if ($tgtPage -le $djvuPage) { 
       Write-Error "Setting djvu $tgtPage after $djvuPage isn't allowed!"
-      # exit 1 
+      exit 1 
     }
-    while ($numeric -and $djvuPage -lt $tgtPage) { Write-Page-Title }
-    $djvuPage, $bookPage = $tgtPage, $matches[2] 
-    if ($matches[3]) { $prefix = $matches[3] }
-    if ($bookPage -match '^\d+$') {
-      $numeric = $true
-      $bookPage = [Int32] $bookPage
-      $offset = $djvuPage - $bookPage
-    } else {
-      $numeric = $false
-      $offset = 0
+    $tgtBook = $matches[2]
+    Inc-LastPage
+    if ($djvuPage -gt 0) {
+      while ($djvuPage -lt $tgtPage) {
+        $djvuPages.Add("$bookPre$bookNum", $djvuPage)
+        [void]$pagesFile.AppendLine("select $djvuPage ; set-page-title `"$bookPre$bookNum`"")
+        Inc-LastPage
+      }
     }
-    Write-Page-Title
+    $djvuPages.Add($tgtBook, $tgtPage)
+    [void]$pagesFile.AppendLine("select $tgtPage ; set-page-title `"$tgtBook`"")
+    $djvuPage = $tgtPage
+    ($bookPre,$bookNum) = Split-BookPage $tgtBook
   }
 
   # Bookmark page
-  '^(\d+)\s+(.*?)\s*$' { 
-    [Int32] $markPage = $matches[1]
-    [void]$marksFile.AppendLine("(`"$($matches[2])`" `"#$($markPage + $offset)`")")
+  '^(\S+)\s+(.*?)\s*$' { 
+    $mpage = $matches[1]
+    $mtitle = $matches[2]
+    if ($mpage -notmatch "^([Dd]jvu|[Mm]eta|#)")  {
+      [void]$marks.Add( @{ "page"=$mpage; "title"=$mtitle } )
+    }
   }
 
   # Anything else is an error!
@@ -59,12 +92,30 @@ switch -Regex -File $src {
 }
 
 # now, write the output if all was well...
-if ($marksFile.Length -gt 0) {
-  [void]$pagesFile.AppendLine("select; set-outline marks.dsed")
-  "(bookmarks`r`n{0})" -f $marksFile.ToString() | Set-Content -LiteralPath marks.dsed -Encoding UTF8NoBOM
-}
 if ($metaFile.Length -gt 0) {
-  [void]$pagesFile.AppendLine("select; set-meta meta.dsed")
-  $metaFile.ToString() | Set-Content -LiteralPath meta.dsed -Encoding UTF8NoBOM
+  [void]$pagesFile.AppendLine("select; set-meta")
+  [void]$pagesFile.Append($metaFile.ToString()) 
+  [void]$pagesFile.AppendLine(".")
 }
-$pagesFile.ToString() | Set-Content -LiteralPath pages.dsed -Encoding UTF8NOBOM
+
+if ($marks.Length -gt 0) {
+  [void]$pagesFile.AppendLine("select; set-outline")
+  [void]$pagesFile.AppendLine("(bookmarks")  
+  foreach ($mark in $marks) {
+    $djp = $djvuPages[$mark["page"]]
+    if (-not $djp) {
+       ($mpPre, $mpNum) = Split-BookPage $mark["page"]
+       if($mpPre) {
+          Write-Error "Bookmark $mpPre$mpNum not found!"
+          exit 1 
+       } else {
+          $djp = $mpNum
+       }
+    }
+    [void]$pagesFile.AppendLine("(`"$($mark["title"])`" `"#$djp`")")
+  }
+  [void]$pagesFile.AppendLine(")")  
+  [void]$pagesFile.AppendLine(".")
+}
+
+$pagesFile.ToString() | Set-Content -LiteralPath "$src.dsed" -Encoding UTF8NoBOM
