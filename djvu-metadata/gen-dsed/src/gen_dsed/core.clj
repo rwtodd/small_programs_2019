147ae0681367cda4910491e67b4b6ddde79f2217
Richard Todd <rwtodd@users.noreply.github.com>
2020-04-29 13:41:58 -0500

    updated clojure version of djvumetadata to match other recent implementations

--- djvu-metadata/gen-dsed/src/gen_dsed/core.clj
+++ djvu-metadata/gen-dsed/src/gen_dsed/core.clj
@@ -8,12 +8,11 @@
    { :rx #"^\s*$|^\s*#.*$" :handler (fn [match] {:type :comment}) }
 
    ;; Meta lines
-   { :rx #"^\s*[Mm][Ee][Tt][Aa]\s+([^:]+):\s*(.*)$"
-    :handler (fn [[all k v]]
-               {:type :meta :kw k :val (.trim v)}) }
+   { :rx #"(?i)^\s*meta\s+([^:]+):\s*(.*?)\s*$"
+    :handler (fn [[all k v]] {:type :meta :kw k :val v}) }
 
    ;; Djvu Pages lines
-   { :rx #"^\s*[Dd][Jj][Vv][Uu]\s+(\d+)\s+(?:Is|is|=)(?:\s+[Bb][Oo][Oo][Kk])?\s+(\S*?)(\d*)\s*$"
+   { :rx #"(?i)^\s*djvu\s+(\d+)\s+(?:is|=)(?:\s+book)?\s+(\S*?)(\d*)\s*$"
     :handler (fn [[all djvu pre num]]
                {:type :djvu
                 :djvu-pg (read-string djvu)
@@ -21,15 +20,16 @@
                 :number (if (empty? num) nil (read-string num))}) }
 
    ;; Bookmark lines -- N.B. this one is permissive, so should be last in the list...
-   { :rx #"^\s*(\S*?)(\d*)\s+(.+)$"
+   { :rx #"^\s*(\S*?)(\d*)\s+(.+?)\s*$"
     :handler (fn [[all pre num name]]
                {:type :mark
                 :numeric (empty? pre)
-                :book-pg (str pre num)
-                :title (.trim name)}) }
+                :prefix (if (empty? pre) nil pre)
+                :number (if (empty? num) nil (read-string num))
+                :title name}) }
    ])
 
-(defn parse-input
+(defn- parse-input
   "Parse a line of input into data" 
   [line]
   (some (fn [{:keys [rx handler]}]
@@ -37,7 +37,7 @@
             (handler m)))
         line-specs))
 
-(defn separate-input
+(defn- separate-input
   "Parse each line in `lines` and separate them by type.
   Make sure all lines parse, and throw an IllegalArgumentException if not.
   Returns the input separated into seqs keyed by :meta :djvu and :marks."
@@ -47,61 +47,90 @@
       (throw (IllegalArgumentException. (str "Line " (inc (.indexOf parsed nil)) " didn't parse!"))))
     (group-by :type parsed)))
 
-(defn next-page
+(defn- next-page
   "Return a spec for the next page, given a page."
   [p]
-  (-> p (update :number #(inc (or % 1))) (update :djvu-pg inc)))
+  (-> p (update :number (fnil inc 1)) (update :djvu-pg inc)))
 
-(defn all-pages
-  "Generate a map from book pages to djvu pages, given the input `djvus` lines"
+(defn- all-pages
+  "Generate a lazy sequence of all pages, given the input `djvus` lines"
   [djvus]
-  (into {}
-        (comp  
-         (mapcat (fn [pair]
-                   (if (== 2 (count pair))
-                     (take-while #(< (:djvu-pg %) (:djvu-pg (second pair))) (iterate next-page (first pair)))
-                     pair)))
-         (map (juxt (fn [pg] (str (:prefix pg) (:number pg))) :djvu-pg)))
-        (partition-all 2 1 djvus)))
+  (->> (partition-all 2 1 djvus)
+       (mapcat (fn [[a b]]
+                 (let [pages (if b
+                               (- (:djvu-pg b) (:djvu-pg a))
+                               1)]
+                   (if (pos? pages)
+                     (take pages (iterate next-page a))
+                     (throw (IllegalArgumentException.
+                             (format "Djvu pg %d isn't after %d!"
+                                     (:djvu-pg b)
+                                     (:djvu-pg a))))))))))
 
-(defn meta->dsed
+(defn- meta->dsed
   "Write dsed-formatted output for `ms` (a seq of meta lines) to `out`"
-  [out ms]
+  [^java.io.BufferedWriter out ms]
   (.write out (format "select; set-meta%n"))
   (doseq [{:keys [kw val]} ms] (.write out (format "%s\t\"%s\"%n" kw val)))
   (.write out (format ".%n")))
 
-(defn marks->dsed
+(defn- marks->dsed
   "Write dsed-formatted output for `ms` (a seq of marks lines) to `out`, using `all-pgs` for reference"
-  [out ms all-pgs]
+  [^java.io.BufferedWriter out ms]
   (.write out (format "select; set-outline%n(bookmarks%n"))
   (doseq [mark ms]
-    (let [djvu-pg (if (:numeric mark)
-                    (:book-pg mark)
-                    (get all-pgs (:book-pg mark)))]
-          (if (nil? djvu-pg)
-            (throw (Exception. (str "Bookmark " mark " not found in the book!")))
-            (.write out (format "(\"%s\" \"#%s\")%n" (:title mark) djvu-pg)))))
+    (.write out (format "  (\"%s\" \"#%s\")%n" (:title mark) (:djvu-pg mark))))
   (.write out (format ")%n.%n")))
 
-(defn pages->dsed
-  "Write dsed-formatted output for `all-pgs` `(the map of page titles) to `out`"
-  [out all-pgs]
-  (doseq [[bp dp] all-pgs]
-    (.write out (format "select %d; set-page-title \"%s\"%n" dp bp))))
+(defn- match-marks
+  "Match bookmarks with the given djvupage, or with itself when they are
+  numeric.  Returns [mm, umm]; matched-marks and unmatched-marks"
+  ([pg marks accum]
+  (let [cur (first marks)]
+    (cond (:numeric cur)
+          (recur pg (rest marks) (conj accum (assoc cur :djvu-pg (:number cur))))
+          (and (= (:number cur) (:number pg))
+               (= (:prefix cur) (:prefix pg)))
+          (recur pg (rest marks) (conj accum (assoc cur :djvu-pg (:djvu-pg pg))))
+          :else [accum marks])))
+  ([pg marks]
+   (match-marks pg marks [])))
 
+(defn- run-through-pages
+  "Print out all the page titles, and during the process, also collect
+  a list of matched bookmarks. This is the most complected function in
+  the program, and I don't see a better way to define it without
+  mutable state."
+  [out pgs mrks]
+  (let [[mm umm] (match-marks {:prefix "dummy" :number -9999} mrks)]
+    (loop [pages   (all-pages pgs)
+           marks   umm
+           matched (transient [mm])]
+      (if (empty? pages)
+        (if (empty? marks)
+          (apply concat (persistent! matched))
+          (throw (Exception. (str "Bookmark <" (:title (first marks)) "> not found!"))))
+        (let [pg       (first pages)
+              [mm umm] (match-marks pg marks)]
+          (.write out (format "select %d; set-page-title \"%s\"%n"
+                              (:djvu-pg pg)
+                              (str (:prefix pg) (:number pg))))
+          (recur (rest pages)
+                 umm
+                 (if (empty? mm) matched (conj! matched mm))))))))
+ 
 (defn -main
   "takes a single arg (a filename), and produces filename.dsed with
   the associated Djvused commands in it."
   [& args]
   (if-not (= (count args) 1)
-    (do (.println *err* "Usage: gen-dsed <filename>")
+    (do (.println ^java.io.PrintWriter *err* "Usage: gen-dsed <filename>")
         (System/exit -1))
-    
     (with-open [input (io/reader (first args))]
       (let [{:keys [mark djvu meta]} (separate-input (line-seq input))
-            pages                    (all-pages djvu)]
-        (with-open [out (io/writer (str (first args) ".dsed"))]
-          (pages->dsed out pages)
-          (when (seq meta) (meta->dsed out meta))
-          (when (seq mark) (marks->dsed out mark pages)))))))
+            out-file (str (first args) ".dsed")]
+        (.println *err* (str "Writing " out-file)) 
+        (with-open [out (io/writer out-file)]
+          (let [matched-marks (run-through-pages out djvu mark)]
+            (when (seq meta) (meta->dsed out meta))
+            (when (seq matched-marks) (marks->dsed out matched-marks))))))))
